2d3dbdd14d9ea1c7ed9f19869c5e55236638fcc5
gi-d-ra <roman4uk.nick@gmail.com>
2020-08-21 20:52:20 +0300

    Add db

---
+++ GraduationProject2020/src/main/resources/db/populateDB.sql
@@ -0,0 +1,27 @@
+DELETE FROM user_roles;
+DELETE FROM votes;
+DELETE FROM users;
+DELETE FROM res_dishes;
+DELETE FROM restaurants;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password) VALUES
+('User', 'user@yandex.ru', '{noop}password'),
+('Admin', 'admin@gmail.com', '{noop}admin');
+
+INSERT INTO user_roles (role, user_id) VALUES
+('USER', 100000),
+('ADMIN', 100001);
+
+INSERT INTO restaurants (name) VALUES ('R1'),('R2'),('R3'),('RWM');
+INSERT INTO res_dishes(name,price,date,res_id) VALUES
+('Pizza',100,'2015-05-30',100005),
+('Cake',200,'2015-05-29',100005);
+
+INSERT INTO votes(res_id,date,user_id) VALUES
+(100005,'2015-05-30',100000),
+(100005,'2015-05-30',100001);
+
+
+
+
